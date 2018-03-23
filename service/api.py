dfa0539faf5f7146de9ef2b7077fbd1fc6aa5093
Humbertzhang <504490160@qq.com>
2018-03-23 16:19:57 +0800

    convert html to plain text in info api

--- service/api.py
+++ service/api.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import ast
 import json
 import redis
@@ -13,7 +14,7 @@ api = Blueprint(
         )
 
 webview_board = redis.StrictRedis(host=os.getenv('WEBVIEW_REDIS_HOST'), port=7388, db=0)
-
+cleanr = re.complile('<.*?>')
 
 @api.route('/webview_info/')
 def api_webview_info():
@@ -33,4 +34,12 @@ def api_webview_info():
 
 @api.route('/info/')
 def api_info():
-    return redirect(url_for('api.api_webview_info')) 
+    webview_board_list = webview_board.get('webview_board_list')
+    if not webview_board_list:
+        board_list = get_webview_board()
+        webview_board.set('webview_board_list', board_list)
+    all_board = ast.literal_eval(webview_board.get('webview_board_list'))
+    # convert html to plain text
+    for item in all_board:
+        item['content'] = re.sub(cleanr, '', item['content'])
+    return jsonify(all_board), 200
